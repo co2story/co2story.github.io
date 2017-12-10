27f396576c8b75b64f96e02d6673b38b97342b1a
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-12-10 21:21:53 +0100

    new graph

--- assets/myd3.js
+++ assets/myd3.js
@@ -82,9 +82,74 @@ function updateMap(currentY) {
     }
   };
   x.send();
+
+}
+
+function createLineChart(country) {
+  // Set the dimensions of the canvas / graph
+  var margin = {top: 30, right: 20, bottom: 30, left: 50},
+  width = 600 - margin.left - margin.right,
+  height = 270 - margin.top - margin.bottom;
+
+  // Set the ranges
+  var x = d3.time.scale().range([0, width]);
+  var y = d3.scale.linear().range([height, 0]);
+
+  // Define the axes
+  var xAxis = d3.svg.axis().scale(x)
+  .orient("bottom").ticks(8);
+
+  var yAxis = d3.svg.axis().scale(y)
+  .orient("left").ticks(5);
+
+  // Define the line
+  var valueline = d3.svg.line()
+  .x(function(d) { return x(d.year); })
+  .y(function(d) { return y(d.value); });
+
+  // Adds the svg canvas
+  var svg = d3.select("#mysvg")
+  .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+      .attr("transform", 
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  // Get the data
+  d3.json(`https://stark-tor-75212.herokuapp.com/api/co2/country?country=${country}`, function(error, data) {
+  data.forEach(function(d) {
+    if(d.value === null)  
+      d.value = 0;
+  });
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.year; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  // Add the valueline path.
+  svg.append("path")
+      .attr("class", "line")
+      .attr("d", valueline(data));
+
+  // Add the X Axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  // Add the Y Axis
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+  });
 }
 
 updateMap(2014);
+createLineChart("China");
+
+var select = document.getElementById("countryChoice");
 var slider = document.getElementById("myRange");
 var output = document.getElementById("curYear");
 output.innerHTML = slider.value; // Display the default slider value
@@ -93,4 +158,9 @@ output.innerHTML = slider.value; // Display the default slider value
 slider.oninput = function() {
     output.innerHTML = this.value;
     updateMap(this.value);
+}
+
+select.onchange = () => {
+  document.getElementById("mysvg").innerHTML = '';
+  createLineChart(select[select.selectedIndex].value);
 }
